673cc24bb60e80352ad8abbc52aef546f558fdde
jonl112 <jonl112@github.com>
2018-01-13 18:33:16 -0800

    Added copy paste functionality

--- NamedPortAssociationConverter.py
+++ NamedPortAssociationConverter.py
@@ -1,3 +1,6 @@
+#new functionality: also converts text in clipboard from a module declaration into a module instatiation
+#with named port association
+
 #this script converts an implicitly defined port association into named port assocation syntax
 #for example: example dut(ain, bin, cout) ==> example dut(.ain(ain), .bin(bin), .cout(cout))
 #also handles line comments for example:
@@ -14,6 +17,7 @@
                 .cout(cout)
                 );
 '''
+import pyperclip
 
 def convert( str ):
     if str =="":
@@ -23,14 +27,14 @@ def convert( str ):
     post = ""
     S = str.split("(")
     str = S[-1]
-    if len(S)==2: 
+    if len(S)==2:
         pre = S[0] +"("
     elif len(S)!=1:
         return "wtf"
     S = str.split(")")
     str = S[0]
-    if len(S)==2: 
-        post = ")" + S[1] 
+    if len(S)==2:
+        post = ")" + S[1]
     elif len(S)!=1:
         return "wtf"
     S = str.split(",")
@@ -38,13 +42,17 @@ def convert( str ):
     for s in S:
         if s.strip():
             outstr += "." + s.strip() + "(" + s.strip() + "), "
-        else: 
+        else:
             outstr += ","
-   
-    return pre + outstr.strip(" ").strip(",") + post 
+
+    return pre + outstr.strip(" ").strip(",") + post
+
+pyperclip.copy(convert(pyperclip.paste()));
+
+
 
 f = open ("data.txt", "r")
-toWrite = "" 
+toWrite = ""
 
 for line in f:
     if line == "\n":
@@ -52,10 +60,10 @@ for line in f:
     else:
         S = line.split("//")
         if len(S)==2:
-            toWrite += convert(S[0]) + "//" + S[1] 
+            toWrite += convert(S[0]) + "//" + S[1]
         else:
             toWrite += convert(line.strip("\n\t ")) + "\n"
-    
+
 f.close()
 
 f = open ("data.txt","w")
--- README.md
+++ README.md
@@ -2,6 +2,6 @@
 converts Verilog module instantiations without named port association into Verilog module instantiations with named port association 
 
 Instructions:
-1. Put your module instantiation in a data.txt file in the same directory as the source code
+1. Put your module instantiation in a data.txt file in the same directory as the source code or copy to clipboard
 2. Run the source code
-3. The result should be in the data.txt
+3. The result should be in the data.txt or in clipboard
