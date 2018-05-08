942e90f2c278c9a05d6513a9c11a3e8d2b0f9470
jonl112 <jonl112@github.com>
2018-05-07 21:32:28 -0700

    added tb converter support and fixed bugs with copy/paste

--- NamedPortAssociationConverter.py
+++ NamedPortAssociationConverter.py
@@ -17,7 +17,7 @@
                 .cout(cout)
                 );
 '''
-import pyperclip
+import pyperclip, sys
 
 def convert( str ):
     if str =="":
@@ -47,7 +47,69 @@ def convert( str ):
 
     return pre + outstr.strip(" ").strip(",") + post
 
-pyperclip.copy(convert(pyperclip.paste()));
+
+def convert_tb( str ):
+    if str =="":
+        return str
+    outstr = ""
+    pre = ""
+    post = ""
+    S = str.split("(")
+    str = S[-1]
+    if len(S)==2:
+        pre = S[0] +"("
+    elif len(S)!=1:
+        return "wtf"
+    S = str.split(")")
+    str = S[0]
+    if len(S)==2:
+        post = ")" + S[1]
+    elif len(S)!=1:
+        return "wtf"
+    S = str.split(",")
+    outstr = ""
+    for s in S:
+        if s.strip():
+            outstr += "." + s.strip() + "(" + "sim_" + s.strip() + "), "
+        else:
+            outstr += ","
+
+    return pre + outstr.strip(" ").strip(",") + post
+
+if (len(sys.argv)==1):
+    f = pyperclip.paste().split("\n")
+    toWrite = ""
+    for line in f:
+        if line == "\n":
+            toWrite += line
+        else:
+            S = line.split("//")
+            if len(S)==2:
+                toWrite += convert(S[0]) + "//" + S[1] + "\n"
+            else:
+                toWrite += convert(line.strip("\n\t ")) + "\n"
+
+    pyperclip.copy(toWrite);
+elif (str(sys.argv[1])=="tb"):
+    f = pyperclip.paste().split("\n")
+    toWrite = ""
+    for line in f:
+        if ("module" in line):
+            short = line[line.find(" ")+1:]
+            bracket = short.split("(")
+            toWrite += bracket[0] + " DUT(" + bracket[1] + "\n"
+
+        elif line == "\n":
+            toWrite += line
+        else:
+            S = line.split("//")
+            if len(S)==2:
+                toWrite += convert_tb(S[0]) + "//" + S[1] + "\n"
+            else:
+                toWrite += convert_tb(line.strip("\n\t ")) + "\n"
+
+    pyperclip.copy(toWrite);
+
 
 
 
